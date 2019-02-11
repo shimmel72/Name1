45c0cb8ebd739e3949e1f0512fef1f9ef4a8f815
12shimmel@gmail.com <>
2019-02-10 23:04:27 -0500

    This is the first of three comments

---
+++ src/name1/Name1.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package name1;
+
+
+public class Name1 {
+
+    
+    public static void main(String[] args) {
+        
+        Shimmel();
+        
+    }
+    
+    public static void Shimmel(){
+        System.out.println("David Shimmel!");
+    }
+    
+}
